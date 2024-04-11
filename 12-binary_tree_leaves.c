5192db930faefcfae412ece876bcbd0ceb835a6a
Xolelwa34 <ciliaswekile@gmail.com>
2024-04-11 05:34:29 -0700

    Nodes

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,12 +7,13 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaf = 0;
+
 	if (!tree)
 		return (0);
-
 	if (!tree->left && !tree->right)
-		return (1);
-
-	return (binary_tree_leaves(tree->left) +
-		binary_tree_leaves(tree->right));
+		leaf += 1;
+	leaf += binary_tree_leaves(tree->left);
+	leaf += binary_tree_leaves(tree->right);
+	return (leaf);
 }
